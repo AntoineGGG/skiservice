85407e009c099131b747fe5496200fb34c584e11
AntoineGGG <a.gorenflot@gmx.fr>
2020-12-24 12:16:24 +0100

    useForm integration pour page de contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout';
 import { useForm } from 'react-hook-form';
+import styles from '../styles/Contact.module.scss';
 
 const Contact = () => {
   const { register, handleSubmit, watch, errors } = useForm();
@@ -7,22 +8,33 @@ const Contact = () => {
 
   return (
     <Layout>
-      <h1>Contactez-nous</h1>
-      <p>
-        Une demande d'informations, de tarifs, disponibilités, n'hésitez pas à
-        nous contacter.
-      </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input name='example' defaultValue='test' ref={register} />
+      <div className={styles.contactContainer}>
+        <h3>Contact</h3>
+        <p>
+          Une demande d'informations, de tarifs, disponibilités, n'hésitez pas à
+          nous contacter.
+        </p>
+      </div>
+      <div className={styles.formContact}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <label>Nom</label>
+          <input name='nom' defaultValue='Votre nom' ref={register} />
+          <label>Prénom</label>
+          <input name='prenom' defaultValue='Votre prénom' ref={register} />
+          <label>Email</label>
+          <input name='email' defaultValue='Votre email' ref={register} />
+          <label>Votre message</label>
+          <textarea />
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input name='exampleRequired' ref={register({ required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+          {/* include validation with required or other standard HTML validation rules */}
+          <input name='exampleRequired' ref={register({ required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
 
-        <input type='submit' />
-      </form>
+          <input type='submit' />
+        </form>
+      </div>
     </Layout>
   );
 };
